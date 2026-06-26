994e07c7cc4d8b0b9ce2dd226a982e2fefd98aec
Nirbhay71 <nirbhayshingala71@gmail.com>
2026-06-26 19:25:14 +0530

    changed google outh's redirect url

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -39,7 +39,7 @@ AuthRouter.get("/google/callback",
                 ...cookieOptions,
                 maxAge: 7 * 24 * 60 * 60 * 1000  // 7 days
             })
-            .redirect(process.env.CORS_ORIGIN || "http://localhost:5173/")
+            .redirect(process.env.CORS_ORIGIN || "https://relay-mesh-git-main-nirbhay71s-projects.vercel.app/")
     }
 )
 
